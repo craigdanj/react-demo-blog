0b6026b3664d7a8b25a9ae01c889d129dfe48e66
craigdanj <craigdanj@gmail.com>
2018-07-05 13:03:08 +0530

    content added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo" />
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
+                <content>
+                </content>
 
             </div>
         );
