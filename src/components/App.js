2fe93c81e696e0cc28f8da3c269818762dd5a79a
craigdanj <craigdanj@gmail.com>
2018-07-05 13:26:01 +0530

    bootstrap added

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
+import PostList from './PostList'
 
 class App extends Component {
     render() {
@@ -11,6 +12,7 @@ class App extends Component {
                     <h1 className="App-title">React demo blog</h1>
                 </header>
 
+                <PostList/>
             </div>
         );
     }
---
+++ src/components/PostList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function PostList(props) {
+
+    return (
+        <div>
+            <ul className="text-left">
+                <li>
+                    Test
+                </li>
+                <li>
+                    Test
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+
+export default PostList;
