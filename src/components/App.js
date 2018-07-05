ace85072d7f922393500ecc8bc1c2923b3c07e01
craigdanj <craigdanj@gmail.com>
2018-07-05 13:40:39 +0530

    post list added

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,46 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import './App.css';
 import PostList from './PostList'
+import axios from 'axios'
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: []
+        }
+
+    }
+
+    componentWillMount() {
+        var postList = [];
+
+        //fetch the current list of todos from the server
+        
+        axios.get("https://jsonplaceholder.typicode.com/todos")
+        .then(response => {
+            // handle success
+            console.log(response.data);
+            postList = response.data.slice(0,10)
+
+            for (var i = postList.length - 1; i >= 0; i--) {
+                postList[i].checked = false;
+                postList[i].text = postList[i].title;
+            }
+
+            this.setState({
+                posts: postList
+            })
+
+            
+        })
+        .catch(error => {
+            // handle error
+            console.log(error);
+        })
+    }
+
     render() {
         return (
             <div className="App">
@@ -12,7 +50,7 @@ class App extends Component {
                     <h1 className="App-title">React demo blog</h1>
                 </header>
 
-                <PostList/>
+                <PostList posts={this.state.posts}/>
             </div>
         );
     }
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 function PostList(props) {
+    let postList = props.posts.map((post, i) => {
+        return <li>{post.title}</li>
+    });
 
     return (
+        
+
         <div>
             <ul className="text-left">
-                <li>
-                    Test
-                </li>
-                <li>
-                    Test
-                </li>
+                {postList}
             </ul>
         </div>
     )
